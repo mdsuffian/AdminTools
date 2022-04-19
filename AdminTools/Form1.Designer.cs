f46a812d8044157b550481887117b43aa9fe5345
mdsuffian <32660866+mdsuffian@users.noreply.github.com>
2022-04-19 15:05:03 +0800

    Add project files.

---
+++ AdminTools/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace AdminTools
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnstart = new System.Windows.Forms.Button();
+            this.btnstop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnstart
+            // 
+            this.btnstart.Location = new System.Drawing.Point(526, 23);
+            this.btnstart.Margin = new System.Windows.Forms.Padding(2);
+            this.btnstart.Name = "btnstart";
+            this.btnstart.Size = new System.Drawing.Size(167, 36);
+            this.btnstart.TabIndex = 0;
+            this.btnstart.Text = "Start";
+            this.btnstart.UseVisualStyleBackColor = true;
+            this.btnstart.Click += new System.EventHandler(this.btnstart_Click);
+            // 
+            // btnstop
+            // 
+            this.btnstop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnstop.Location = new System.Drawing.Point(718, 23);
+            this.btnstop.Margin = new System.Windows.Forms.Padding(2);
+            this.btnstop.Name = "btnstop";
+            this.btnstop.Size = new System.Drawing.Size(167, 36);
+            this.btnstop.TabIndex = 1;
+            this.btnstop.Text = "Stop";
+            this.btnstop.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1476, 588);
+            this.Controls.Add(this.btnstop);
+            this.Controls.Add(this.btnstart);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnstart;
+        private System.Windows.Forms.Button btnstop;
+    }
+}
+
---
+++ AdminTools/Form1.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.Web.Administration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace AdminTools
+{
+    public partial class Form1 : Form
+    {
+
+        public DataSet dsIIS = new DataSet();
+        public DataTable dtIIS = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnstart_Click(object sender, EventArgs e)
+        {
+            PrepareDataColumn();
+
+            string serverlst = ConfigurationManager.AppSettings["ServerList"];
+            List<string> lstserver = serverlst.Split(',').ToList<string>();
+            foreach (string servervar in lstserver)
+            {
+                
+                using (var mgr = ServerManager.OpenRemote(servervar))
+                {
+                    foreach (Site site in mgr.Sites.ToList())
+                    {
+                        
+                        int siteport = site.Bindings.Where(x => x.Protocol == "http").Select(y => y.EndPoint.Port).FirstOrDefault();
+
+                        var logfile = site.LogFile;
+                        string sitename = site.Name;
+
+                        foreach (Microsoft.Web.Administration.Application app in site.Applications.ToList())
+                        {
+                            string AppPath = app.Path.ToString();
+                            string ApppoolName = app.ApplicationPoolName;
+                            string RuntimeVer = mgr.ApplicationPools[ApppoolName].ManagedRuntimeVersion;
+                            bool enable32bit = mgr.ApplicationPools[ApppoolName].Enable32BitAppOnWin64;
+                            string managedpipeline = mgr.ApplicationPools[ApppoolName].ManagedPipelineMode.ToString();
+                            string IdentityType = mgr.ApplicationPools[ApppoolName].ProcessModel.IdentityType.ToString();
+                            string IdentityUser = mgr.ApplicationPools[ApppoolName].ProcessModel.UserName.ToString();
+
+                            DataRow drIIS = dtIIS.NewRow();
+                            drIIS["servername"] = servervar;
+                            drIIS["sitename"] = sitename;
+                            drIIS["port"] = siteport;
+                            drIIS["apppath"] = AppPath;
+                            drIIS["apppoolname"] = ApppoolName;
+                            drIIS["RuntimeVer"] = RuntimeVer;
+                            drIIS["enable32bit"] = enable32bit;
+                            drIIS["managedpipelinemode"] = managedpipeline;
+                            drIIS["IdentityType"] = IdentityType;
+                            drIIS["IdentityUser"] = IdentityUser;
+                            drIIS["PhysicalPath"] = app.VirtualDirectories[0].PhysicalPath;
+                            drIIS["URLPath"] = @"http://" + servervar + @":" + siteport + AppPath;
+
+                            
+                            if (app.VirtualDirectories[0].PhysicalPath.Contains(':'))
+                            {
+                                string configpath = @"\\" + servervar + @"\" + app.VirtualDirectories[0].PhysicalPath.Split(':')[0] + @"$" + app.VirtualDirectories[0].PhysicalPath.Split(':')[1] + @"\web.config";
+                                ExeConfigurationFileMap configMap = new ExeConfigurationFileMap();
+                                configMap.ExeConfigFilename = configpath;
+                                System.Configuration.Configuration config = ConfigurationManager.OpenMappedExeConfiguration(configMap, ConfigurationUserLevel.None);
+
+                                try
+                                {
+
+
+                                    if (config.ConnectionStrings.ConnectionStrings.Count > 0)
+                                    {
+                                        List<string> datas = new List<string>();
+
+                                        foreach (ConnectionStringSettings con in config.ConnectionStrings.ConnectionStrings)
+                                        {
+                                            if (!(con.ConnectionString.Contains("Microsoft.Jet.OLEDB")))
+                                            {
+
+
+                                                var csb = new SqlConnectionStringBuilder(con.ConnectionString);
+
+
+                                                if (!(csb.DataSource == @".\SQLEXPRESS" || csb.DataSource == ""))
+                                                {
+                                                    if (!(datas.Contains(csb.DataSource.ToLower())))
+                                                    {
+
+
+
+                                                        datas.Add(csb.DataSource.ToLower().Replace("data source=", ""));
+                                                    }
+
+                                                }
+                                            }
+
+
+                                        }
+
+
+                                        drIIS["DataConnection"] = string.Join(",", datas.ToArray());
+
+                                        dtIIS.Rows.Add(drIIS);
+                                    }
+                                }
+                                catch (ConfigurationException ce)
+                                {
+
+                                }
+                                catch (Exception ex)
+                                {
+
+
+                                }
+
+                            }
+
+                        }
+                    }
+                }
+
+            }
+
+
+            //dataGridView1.DataSource = dtIIS;
+
+        }
+
+        private void PrepareDataColumn()
+        {
+            DataColumn dcservername = new DataColumn("servername");
+            DataColumn dcsitename = new DataColumn("sitename");
+            DataColumn dcport = new DataColumn("port");
+            DataColumn dcapppath = new DataColumn("apppath");
+            DataColumn dcapppoolname = new DataColumn("apppoolname");
+            DataColumn dcRuntimeVer = new DataColumn("RuntimeVer");
+            DataColumn dcenable32bit = new DataColumn("enable32bit");
+            DataColumn dcmanagedpipelinemode = new DataColumn("managedpipelinemode");
+            DataColumn dcIdentityType = new DataColumn("IdentityType");
+            DataColumn dcIdentityUser = new DataColumn("IdentityUser");
+            DataColumn dcPhysicalPath = new DataColumn("PhysicalPath");
+            DataColumn dcURLPath = new DataColumn("URLPath");
+            DataColumn dcDataConnection = new DataColumn("DataConnection");
+
+            dtIIS.Columns.Add(dcservername);
+            dtIIS.Columns.Add(dcsitename);
+            dtIIS.Columns.Add(dcport);
+            dtIIS.Columns.Add(dcapppath);
+            dtIIS.Columns.Add(dcapppoolname);
+            dtIIS.Columns.Add(dcRuntimeVer);
+            dtIIS.Columns.Add(dcenable32bit);
+            dtIIS.Columns.Add(dcmanagedpipelinemode);
+            dtIIS.Columns.Add(dcIdentityType);
+            dtIIS.Columns.Add(dcIdentityUser);
+            dtIIS.Columns.Add(dcPhysicalPath);
+            dtIIS.Columns.Add(dcURLPath);
+            dtIIS.Columns.Add(dcDataConnection);
+        }
+    }
+}
+
